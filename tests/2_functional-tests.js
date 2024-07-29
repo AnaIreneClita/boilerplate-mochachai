53c00ce9103b478109409edc6766dd7aa3f60f48
Ana Clita <ana.irene.second@gmail.com>
2024-07-29 14:00:23 +0000

    Solved functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,91 +1,118 @@
 const chai = require('chai');
 const assert = chai.assert;
-
+const chaiHttp = require('chai-http');
+const Browser = require('zombie');
 const server = require('../server');
 
-const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
+
+const browser = new Browser();
 
 suite('Functional Tests', function () {
   this.timeout(5000);
+
   suite('Integration tests with chai-http', function () {
     // #1
     test('Test GET /hello with no name', function (done) {
       chai
         .request(server)
-        .keepOpen()
         .get('/hello')
         .end(function (err, res) {
+          if (err) return done(err);
           assert.equal(res.status, 200, 'Response status should be 200');
           assert.equal(res.text, 'hello Guest', 'Response should be "hello Guest"');
           done();
         });
     });
+
     // #2
     test('Test GET /hello with your name', function (done) {
       chai
         .request(server)
-        .keepOpen()
         .get('/hello?name=Ana')
         .end(function (err, res) {
+          if (err) return done(err);
           assert.equal(res.status, 200, 'Response status should be 200');
           assert.equal(res.text, 'hello Ana', 'Response should be "hello Ana"');
           done();
         });
     });
+
     // #3
     test('Send {surname: "Colombo"}', function (done) {
       chai
         .request(server)
-        .keepOpen()
         .put('/travellers')
-
+        .send({ surname: 'Colombo' })
         .end(function (err, res) {
+          if (err) return done(err);
           assert.equal(res.status, 200, 'Response status should be 200');
           assert.isObject(res.body, 'Response body should be an object');
-          assert.property(res.body, 'message', 'Response body should contain a `message` property');
-          assert.equal(res.body.message, 'Surname updated to Colombo', 'Response message should be "Surname updated to Colombo"');
-
+          assert.property(res.body, 'name', 'Response body should contain a `name` property');
+          assert.property(res.body, 'surname', 'Response body should contain a `surname` property');
+          assert.equal(res.body.name, 'Cristoforo', 'Response body `name` should be "Cristoforo"');
+          assert.equal(res.body.surname, 'Colombo', 'Response body `surname` should be "Colombo"');
           done();
         });
     });
+
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.equal(res.status, 200, 'Response status should be 200');
-      assert.isObject(res.body, 'Response body should be an object');
-      assert.property(res.body, 'message', 'Response body should contain a `message` property');
-      assert.equal(res.body.message, 'Surname updated to da Verrazzano', 'Response message should be "Surname updated to da Verrazzano"');
-          
-      done();
+      chai
+        .request(server)
+        .put('/travellers')
+        .send({ surname: 'da Verrazzano' })
+        .end(function (err, res) {
+          if (err) return done(err);
+          assert.equal(res.status, 200, 'Response status should be 200');
+          assert.isObject(res.body, 'Response body should be an object');
+          assert.property(res.body, 'message', 'Response body should contain a `message` property');
+          assert.equal(res.body.message, 'Surname updated to da Verrazzano', 'Response message should be "Surname updated to da Verrazzano"');
+          done();
+        });
     });
   });
 });
 
-const Browser = require('zombie');
-
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
 
-
-
   suite('Headless browser', function () {
-    test('should have a working "site" property', function() {
-      assert.isNotNull(browser.site);
+    test('should have a working "site" property', function (done) {
+      browser.visit('http://localhost:3000')
+        .then(function () {
+          assert.isNotNull(browser.site);
+          done();
+        })
+        .catch(done);
     });
   });
 
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.equal(browser.text('body'), 'Expected response after submitting "Colombo"');
-
-      done();
+      browser.visit('http://localhost:3000/form')
+        .then(function () {
+          return browser.fill('surname', 'Colombo').pressButton('Submit');
+        })
+        .then(function () {
+          assert.include(browser.text('body'), 'Expected response after submitting "Colombo"');
+          done();
+        })
+        .catch(done);
     });
+
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.equal(browser.text('body'), 'Expected response after submitting "Vespucci"');
-
-      done();
+      browser.visit('http://localhost:3000/form')
+        .then(function () {
+          return browser.fill('surname', 'Vespucci').pressButton('Submit');
+        })
+        .then(function () {
+          assert.include(browser.text('body'), 'Expected response after submitting "Vespucci"');
+          done();
+        })
+        .catch(done);
     });
   });
 });
